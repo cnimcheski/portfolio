e3c77d2ed7ec03bcd6a76b42234eb8091b45fc51
cnimcheski <cnimcheski@hotmail.com>
2024-08-24 19:30:56 -0600

    Added some improvements to personal info

--- javascript/index.js
+++
@@ -1,7 +0,0 @@
-import '@material/web/button/filled-button.js';
-import '@material/web/button/outlined-button.js';
-import '@material/web/checkbox/checkbox.js';
-
-import '@material/web/fab/branded-fab.js';
-import '@material/web/fab/fab.js';
-import '@material/web/icon/icon.js';
